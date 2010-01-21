77db14104296e340ff6ce1073891b07881201daf
Piotr Zalewa <piotr@zalewa.info>
2010-01-21 18:09:06 +0000

    basic file structure

---
+++ flightdeck/settings_local-default.py
@@ -0,0 +1,17 @@
+ADMINS = (
+   # ('Your Name', 'your_email@domain.com'),
+)
+
+DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+SECRET_KEY = '_878&mu1t!-d*u^*@l$afwe$p4r(=*$kyyjy37ibf9t8li5#lv'
