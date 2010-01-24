6ae9ebadf0887a6cf3869db92523c17e5692601e
zalun <piotr@zalewa.info>
2010-01-24 16:43:56 +0000

    licenses updated, test for docutils (admin automatic documentation)

--- flightdeck/base/views.py
+++ flightdeck/base/views.py
@@ -1,4 +1,9 @@
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 
 def placeholder(req):
+	"""
+	Display simple text. Just a proof the system is working
+
+	@return HttpResponse: "<h1>Hello World</h1>"
+	"""
 	return HttpResponse("<h1>Hello World</h1>")
