b862ff7fd17e12c890e43fd50b047d74bead8a6d
zalun <piotr@zalewa.info>
2010-02-12 18:12:48 +0000

    redirect to main page after logout

--- flightdeck/person/urls.py
+++ flightdeck/person/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('person.views',
 	url(r'^login/$', login, name='login'),
-	url(r'^logout/$', logout, name='logout'),
+	url(r'^logout/$', logout, {"next_page": "/"}, name='logout'),
     url(r'^(?P<username>\w+)/$','public_profile', name='person_public_profile'),
 )
 
