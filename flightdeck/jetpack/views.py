1220901be8c602ffb29806265c82310aae19ef7a
zalun <piotr@zalewa.info>
2010-02-12 16:45:36 +0000

    logging in

--- flightdeck/jetpack/views.py
+++ flightdeck/jetpack/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render_to_response, get_object_or_404
 #from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.template import RequestContext#,Template
+from django.contrib.auth.decorators import login_required
+
 
 from jetpack.models import Jetpack, Version
 from jetpack.default_settings import settings
 
-
+@login_required
 def edit_new(r):
 	return render_to_response('edit_new.html', {}, 
 				context_instance=RequestContext(r))
 	
 
+@login_required
 def edit_base(r, slug):
 	jetpack = get_object_or_404(Jetpack, slug=slug)
 	version = jetpack.base_version
@@ -18,6 +21,8 @@ def edit_base(r, slug):
 	return render_to_response('edit.html', locals(), 
 				context_instance=RequestContext(r))
 	
+
+@login_required
 def edit_version(r, slug, version):
 	version = get_object_or_404(Version, jetpack__slug=slug, name=version)
 	jetpack = version.jetpack
--- flightdeck/person/urls.py
+++ flightdeck/person/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import login, logout
+
 
 urlpatterns = patterns('person.views',
+	url(r'^login/$', login, name='login'),
+	url(r'^logout/$', logout, name='logout'),
     url(r'^(?P<username>\w+)/$','public_profile', name='person_public_profile'),
 )
 
