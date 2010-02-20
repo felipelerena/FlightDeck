603216f2ac5d3de325cc40d25c0f7ad22d9c547b
Oskar Krawczyk <oskar.krawczyk@gmail.com>
2010-02-20 18:47:06 +0000

    More styles and structural changes

--- flightdeck/jetpack/media/js/Sidebar.js
+++ flightdeck/jetpack/media/js/Sidebar.js
@@ -4,34 +4,44 @@
 
 
 var Sidebar = new Class({
+	
 	Implements: [Options, Events],
+	
 	options: {
 		togglerTrigger: '.UI_Sidebar_Toggler a',
 		togglerContainer: '.UI_Sidebar_ItemCont'
 	},
 	
-	initialize: function(options) {
+	initialize: function(options){
 		this.setOptions(options);
 		this.checkTogglers();
 	},
 	
-	checkTogglers: function() {
+	checkTogglers: function(){
 		this.togglers = $$(this.options.togglerTrigger);
 		this.containers = $$(this.options.togglerContainer);
 		this.slideFx = [];
-		var self = this;
 		
-		this.containers.each(function(container, index){
-			this.slideFx[index] = new Fx.Slide(container).hide();
-			this.togglers[index].getParent().addClass('closed');
-			this.togglers[index].addEvents({
-				click: function(e){
-					e.stop();
-					this.getParent().toggleClass('closed');
-					self.slideFx[index].toggle();
-				}
-			});
-		}, this);
+		this.containers.each(this.attachActions.bind(this));
+	},
+	
+	attachActions: function(container, index){
+		var self = this,
+			currentToggler = this.togglers[index].getParent();
+		
+		this.slideFx[index] = new Fx.Slide(container);
+		
+		if (currentToggler.hasClass('closed')){
+			this.slideFx[index].hide();
+		}
+		
+		this.togglers[index].addEvents({
+			click: function(e){
+				e.stop();
+				this.getParent().toggleClass('closed');
+				self.slideFx[index].toggle();
+			}
+		});
 	}
 });
 
