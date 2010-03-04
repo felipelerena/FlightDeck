b6bccbda368e24900a7d6726d8ba397977c47d49
zalun <piotr@zalewa.info>
2010-03-04 12:15:00 +0000

    Roar refactored - ability to position in the center horizontally

--- flightdeck/media/js/FlightDeck.Roar.js
+++ flightdeck/media/js/FlightDeck.Roar.js
@@ -1,3 +1,41 @@
+
+/* 
+ * File: Flightdeck.Roar.js
+ */
+
+/* 
+ * Extending Roar possibilities
+ */
+Roar = Class.refactor(Roar, {
+	reposition: function() {
+		var max = document.getCoordinates(), scroll = document.getScroll(), margin = this.options.margin;
+		max.left += scroll.x;
+		max.right += scroll.x;
+		max.top += scroll.y;
+		max.bottom += scroll.y;
+		var rel = ($type(this.container) == 'element') ? this.container.getCoordinates() : max;
+		var left;
+		if (this.position.x == 'right') {
+			left = (Math.min(rel.right, max.right) - margin.x);
+		} else if (this.position.x == 'center') {
+			// TODO: HACK: 140 is half of the box width
+			left = Math.round((Math.min(rel.right, max.right) / 2 - 140));
+		} else {
+			left = (Math.max(rel.left, max.left) + margin.x);
+		}
+		var fromtop = (this.position.y == 'bottom')
+				? (Math.min(rel.bottom, max.bottom) - margin.y)
+				: (Math.max(rel.top, max.top) + margin.y);
+
+
+		this.moveTo({
+			left: left,
+			top: fromtop
+		});
+	}
+	
+});
+
 /*
  * Extending Flightdeck with Roar messages
  */
@@ -8,13 +46,16 @@ FlightDeck = Class.refactor(FlightDeck,{
 		this.previous(options);
 		
 		this.warning = new Roar({
-			className: 'roar warning'
+			className: 'roar warning',
+			position: 'topCenter',
 		});
 		this.error = new Roar({
+			position: 'topLeft',
 			className: 'roar error',
-			duration: 50000
+			duration: 5000
 		});
 		this.message = new Roar({
+			position: 'topRight',
 			className: 'roar warning'
 		});
 		this.parseMessages();
