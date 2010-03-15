a55a93d4fd7ad6b0406714877621b07ae41848fe
zalun <piotr@zalewa.info>
2010-03-15 13:47:21 +0000

    default settings fixed

--- flightdeck/jetpack/default_settings.py
+++ flightdeck/jetpack/default_settings.py
@@ -4,4 +4,4 @@ from django.conf import settings
 # TODO: make things work if True
 JETPACK_NEW_IS_BASE = False
 
-JETPACK_ITEMS_PER_PAGE = 2#getattr(settings, 'JETPACK_ITEMS_PER_PAGE', 2)
+JETPACK_ITEMS_PER_PAGE = getattr(settings, 'JETPACK_ITEMS_PER_PAGE', 10)
