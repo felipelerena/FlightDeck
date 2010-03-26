d15db3bb8b3e2237dfe07aa08bf5a94ef896c985
zalun <piotr@zalewa.info>
2010-03-26 20:38:04 +0000

    check if extension installed

--- flightdeck/media/js/FlightDeck.js
+++ flightdeck/media/js/FlightDeck.js
@@ -99,9 +99,11 @@ Class.Mutators.$name = function(name){ this.implement('$family', {name: name}); 
 	Listen to an event fired when Extension is installed
  */
 window.addEvent('load', function() {
-	window.mozFlightDeck.whenMessaged(function(data) {
-		// This gets called when one of our extensions has been installed
-		// successfully, or failed somehow.
-		fd.message.alert('Loading extension', 'Extension {msg}'.substitute(data));
-	});
+	if (fd.alertIfNoAddOn()) {
+		window.mozFlightDeck.whenMessaged(function(data) {
+			// This gets called when one of our extensions has been installed
+			// successfully, or failed somehow.
+			fd.message.alert('Loading extension', 'Extension {msg}'.substitute(data));
+		});
+	}
 });
