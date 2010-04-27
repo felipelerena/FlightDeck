3e7c855851a4dd18fec76c38640195762fdb6555
zalun <zalun@neverland.(none)>
2010-04-27 13:37:48 +0100

    dev server not needed

--- scripts/rundevserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source scripts/environment.sh
-
-# run server
-cd $PROJECT_DIR/$PROJECT_NAME/
-$PYTHON_COMMAND ./manage.py rundevserver localhost:8090 --adminmedia=$PROJECT_DIR/$PROJECT_NAME/adminmedia/
