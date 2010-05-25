2aeb4c16c1ccfb133699bccad00b671952f8cf94
Piotr Zalewa <piotr@zalewa.info>
2010-05-25 10:42:16 +0100

    displaying versions and revisions - URL hooks

--- flightdeck/jetpack/urls.py
+++ flightdeck/jetpack/urls.py
@@ -3,5 +3,19 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('jetpack.views',
 	url(r'^addons/$', 'package_browser', {'type': 'a'}, name='jp_browser_addons'),
 	url(r'^libraries/$', 'package_browser', {'type': 'l'}, name='jp_browser_libraries'),
+
+	# display details of the PackageRevision
+	url(r'^addon/(?P<id>[-\w]+)/$', 
+		'package_details', {'type': 'a'}, name='jp_addon_details'),
+	url(r'^library/(?P<id>[-\w]+)/$', 
+		'package_details',{'type': 'l'},  name='jp_library_details'),
+	url(r'^addon/(?P<id>[-\w]+)/version/(?P<version_name>.*)/$', 
+		'package_details', {'type': 'a'}, name='jp_addon_details'),
+	url(r'^library/(?P<id>[-\w]+)/version/(?P<version_name>.*)/$', 
+		'package_details',{'type': 'l'},  name='jp_library_details'),
+	url(r'^addon/(?P<id>[-\w]+)/revision/(?P<revision_number>\d+)/$', 
+		'package_details', {'type': 'a'}, name='jp_addon_details'),
+	url(r'^library/(?P<id>[-\w]+)/revision/(?P<revision_number>\d+)/$', 
+		'package_details',{'type': 'l'},  name='jp_library_details'),
 )
 
--- flightdeck/jetpack/views.py
+++ flightdeck/jetpack/views.py
@@ -52,3 +52,24 @@ def package_browser(r, page_number=1, type=None):
 		},
 		context_instance=RequestContext(r))
 
+def package_details(r, id, type, revision_number=None, version_name=None):
+
+	if not (revision_number or version_name):
+		# get default revision - one linked via Package:version
+		package = get_object_with_related_or_404(Package, id_number=id, type=type)
+		package_revision = package.version
+
+	elif revision_number:
+		# get version given by revision number
+		package_revision = get_object_with_related_or_404(PackageRevision, 
+							package__id_number=id, package__type=type,
+							revision_number=revision_number)
+	elif version_name:
+		# get version given by version name
+		package_revision = get_object_with_related_or_404(PackageRevision, 
+							package__id_number=id, package__type=type,
+							version_name=version_name)
+
+	return HttpResponse(package_revision.__unicode__())
+		
+
