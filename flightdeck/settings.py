be3cbabda0543cd353753ad1cca63be71d807942
Piotr Zalewa <piotr@zalewa.info>
2010-05-27 23:06:47 +0100

    middleware preventing from POST attacks and messages added (django 1.2)

--- flightdeck/settings.py
+++ flightdeck/settings.py
@@ -70,7 +70,9 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
 	'django.middleware.common.CommonMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 	'debug_toolbar.middleware.DebugToolbarMiddleware',
 ) 
 
