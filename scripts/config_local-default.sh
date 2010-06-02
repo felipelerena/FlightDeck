a18ca425041ad970f525557ef4c4826a9da5652b
Piotr Zalewa <piotr@zalewa.info>
2010-06-02 10:25:49 +0100

    no slash and more descriptive

--- scripts/config_local-default.sh
+++ scripts/config_local-default.sh
@@ -5,8 +5,10 @@ source $BASEDIR/config.sh
 
 PYTHON_ENVIRONMENT=$PROJECT_NAME
 PYTHON_COMMAND=python
+
 # define directories
-PROJECT_DIR='/path/to/project/'
+PROJECT_DIR='/path/to/FlightDeck'
 V_ENV=$PROJECT_DIR/flightdeckenv
+
 # that's for graphviz
 export TMP=/tmp/
